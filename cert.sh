74b749b06fb5fc267d21a278865141b7571e1d49
david.speakman <david.speakman@innovapost.com>
2022-02-15 09:22:44 -0500

    commit

--- cert.sh
+++ cert.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-keytool -genkey -alias broker -keyalg RSA -keystore broker.ks -storepass XXXXXX -dname CN=$AMQ_MQTT_ROUTE_HOST
-keytool -export -alias broker -keystore broker.ks -file broker_cert -storepass XXXXXX 
-keytool -genkey -alias client -keyalg RSA -keystore client.ks -storepass XXXXXX -dname CN=localhost
-keytool -import -alias broker -keystore client.ts -file broker_cert -storepass XXXXXX -noprompt
+keytool -genkey -alias broker -keyalg RSA -keystore broker.ks -storepass aropoc -dname CN=amq-mqtt-mqtt-0-svc-rte-amq-mqtt.apps.r7dmod0x.canadaeast.aroapp.io
+keytool -export -alias broker -keystore broker.ks -file broker_cert -storepass aropoc 
+keytool -genkey -alias client -keyalg RSA -keystore client.ks -storepass aropoc -dname CN=localhost
+keytool -import -alias broker -keystore client.ts -file broker_cert -storepass aropoc -noprompt
 oc create secret generic amq-mqtt-mqtt-secret \
 --from-file=broker.ks=broker.ks \
 --from-file=client.ts=broker.ks \
---from-literal=keyStorePassword=XXXXXX \
---from-literal=trustStorePassword=XXXXXX \
+--from-literal=keyStorePassword=aropoc \
+--from-literal=trustStorePassword=aropoc \
  -n amq-mqtt
 
  oc secrets link sa/amq-broker-operator secret/amq-mqtt-mqtt-secret -n amq-mqtt
