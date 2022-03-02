2ffe117238684deb32b29ea8f9c10a806e9d3d15
davidspeakman <99036022+davidspeakman@users.noreply.github.com>
2022-03-02 12:25:17 -0500

    Update ActiveMqRoute.java
    Updated to allow for dynamic wc and route from the PDTs.

--- client-activemq/src/main/java/org/example/activemq/ActiveMqRoute.java
+++ client-activemq/src/main/java/org/example/activemq/ActiveMqRoute.java
@@ -1,5 +1,6 @@
 package org.example.activemq;
 
+import org.apache.activemq.command.ActiveMQTopic;
 import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -10,10 +11,15 @@ public class ActiveMqRoute extends EndpointRouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        from(activemq("topic:goc_cpc.stg.apps.dia.events.newstop.item.wc.*.route.*")).routeId("ActiveMQ")
+        from(activemq("topic:goc_cpc.std.apps.dia.events.newstop.item.wc.*.route.*")).routeId("ActiveMQ")
                 .log("${body}")
-                .setBody(simple("${body} + ' + response'"))
-                .to(activemq("topic:goc_cpc.stg.apps.dia.notifications.purequest.pdt.*.wc1234.route.*.puassign"));
+                .process(e -> {
+                    ActiveMQTopic topic = e.getIn().getHeader("JMSDestination", ActiveMQTopic.class);
+                    e.getIn().setHeader("wc", topic.getTopicName().split("\\.")[8]);
+                    e.getIn().setHeader("route", topic.getTopicName().split("\\.")[10]);
+                })
+                .setBody(simple("${body} ' + response'"))
+                .toD(activemq("topic:reply.wc.${header.pdt}.route.${header.route}"));
     }
 
 }
