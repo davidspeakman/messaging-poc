603c92b0dfb246c72c85967e542cc5ce78fac467
davidspeakman <99036022+davidspeakman@users.noreply.github.com>
2022-03-02 15:39:53 -0500

    Update MqttProducerRoute.java

--- client-mqtt/src/main/java/org/example/mqtt/MqttProducerRoute.java
+++ client-mqtt/src/main/java/org/example/mqtt/MqttProducerRoute.java
@@ -12,10 +12,10 @@ public class MqttProducerRoute extends EndpointRouteBuilder {
     public void configure() throws Exception {
 
         Properties properties = new Properties();
-        properties.setProperty("com.ibm.ssl.trustStore", "../../client.ts");
-        properties.setProperty("com.ibm.ssl.trustStorePassword", "123456");
-        properties.setProperty("com.ibm.ssl.keyStore", "../../client.ks");
-        properties.setProperty("com.ibm.ssl.keyStorePassword", "123456");
+        properties.setProperty("com.ibm.ssl.trustStore", "C:\\dev\\aroCluster\\poc\\messaging-poc\\client.ts");
+        properties.setProperty("com.ibm.ssl.trustStorePassword", "aropoc");
+        properties.setProperty("com.ibm.ssl.keyStore", "C:\\dev\\aroCluster\\poc\\messaging-poc\\client.ks");
+        properties.setProperty("com.ibm.ssl.keyStorePassword", "aropoc");
 
         from(timer("demo").period(2000).repeatCount(3000)).routeId("Producer")
                 .setBody(simple("Hello World from {{pdt}}"))
