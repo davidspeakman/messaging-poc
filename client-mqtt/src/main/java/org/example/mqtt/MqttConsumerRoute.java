7396e2d97cff37c230a6ff4a87ef44f8a4d46687
davidspeakman <99036022+davidspeakman@users.noreply.github.com>
2022-03-02 15:05:51 -0500

    Update MqttConsumerRoute.java

--- client-mqtt/src/main/java/org/example/mqtt/MqttConsumerRoute.java
+++ client-mqtt/src/main/java/org/example/mqtt/MqttConsumerRoute.java
@@ -12,12 +12,12 @@ public class MqttConsumerRoute extends EndpointRouteBuilder {
     public void configure() throws Exception {
 
         Properties properties = new Properties();
-        properties.setProperty("com.ibm.ssl.trustStore", "C:\\dev\\aroCluster\\poc\\messaging-poc\\client.ts");
-        properties.setProperty("com.ibm.ssl.trustStorePassword", "aropoc");
-        properties.setProperty("com.ibm.ssl.keyStore", "C:\\dev\\aroCluster\\poc\\messaging-poc\\client.ks");
-        properties.setProperty("com.ibm.ssl.keyStorePassword", "aropoc");
+        properties.setProperty("com.ibm.ssl.trustStore", "../../client.ts");
+        properties.setProperty("com.ibm.ssl.trustStorePassword", "123456");
+        properties.setProperty("com.ibm.ssl.keyStore", "../../client.ks");
+        properties.setProperty("com.ibm.ssl.keyStorePassword", "123456");
 
-        from(paho("goc_cpc.stg.apps.dia.notifications.purequest.pdt.wc.wc1234.route.r1234.puassign").clientId("mqtt-consumer").cleanSession(true).sslClientProps(properties)).routeId("Consumer")
+        from(paho("reply.wc.{{pdt}}.route.{{pdt}}").cleanSession(true).sslClientProps(properties)).routeId("Consumer")
                 .log("${body}");
     }
 
